adb11b10df9ad146d6e6d516bab934bf674b8b4a
TeaCondemns <tea.condemns@gmail.com>
2023-01-15 16:58:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from pygame.constants import K_z, K_l, K_s, K_F1, K_F11
 from pygex import Window, Toast, GK_CTRL, GK_SHIFT
 from projectmanager import ProjectManager
-from pygex.color import GREEN, WHITE
+from pygex.color import C_GREEN, C_WHITE
 from core.recorder import Recorder
 from core.canvas import Canvas
 from core.pen import Pen
 
 
 window = Window(title='VectorPaint')
-window.bg_color = WHITE
+window.bg_color = C_WHITE
 window.fps_limit = 120
 
 fullscreen_toast = Toast('To exit full screen press [F11]')
 
 canvas = Canvas()
 pen = Pen(canvas)
-pen.color = GREEN
+pen.color = C_GREEN
 
 recorder = Recorder(canvas)
 project_manager = ProjectManager(canvas)
